f50f25ac0455b30cf3d85145b578520297dc55e4
Roham Pardakhtim <roham.p@gmail.com>
2018-03-13 00:57:28 -0700

    Added homework3

---
+++ homework3/.xaml.cs
@@ -0,0 +1,59 @@
+﻿// CSHP220 
+// Assignment 3
+// By: Roham Pardakhtim
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        CollectionView view;
+
+        public SecondWindow()
+        {
+            InitializeComponent();
+
+            // create a list of users to show up on List View Control
+            var users = new List<Models.User>();
+
+            users.Add(new Models.User { Name = "Dave", Password = "DavePwd" });
+            users.Add(new Models.User { Name = "1Steve", Password = "StevePwd" });
+            users.Add(new Models.User { Name = "Lisa", Password = "1isaPwd" });
+
+            uxList.ItemsSource = users;
+            view = (CollectionView)CollectionViewSource.GetDefaultView(uxList.ItemsSource);
+
+        }
+
+        //sort listview by Name
+        private void GridViewColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            view.SortDescriptions.Clear();
+            view.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
+        }
+        
+        //sort listview by Password
+        private void GridViewColumnHeader_Click_1(object sender, RoutedEventArgs e)
+        {
+            view.SortDescriptions.Clear();
+            view.SortDescriptions.Add(new SortDescription("Password", ListSortDirection.Ascending));
+        }
+    }
+}
---
+++ homework3/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        // handle the exception globally
+        public App()
+        {
+            Application.Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
+        }
+
+        private void Current_DispatcherUnhandledException(object sender,
+                               System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            Exception ex = (Exception)e.Exception;
+            MessageBox.Show(ex.Message, "Unhandled Exception");
+            e.Handled = true; // this allows the application to continue to run and not shut down
+        }
+    }
+}
