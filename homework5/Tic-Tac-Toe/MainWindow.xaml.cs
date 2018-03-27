cea4f6a4e4c553ef485aaf00496397f869e32d22
Roham Pardakhtim <roham.p@gmail.com>
2018-03-27 02:15:23 -0700

    Added homework5

---
+++ homework5/Tic-Tac-Toe/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿// CSHP220 
+// Assignment 5
+// By: Roham Pardakhtim
+
+using System.Windows;
+using System.Windows.Controls;
+using Tic_Tac_Toe.Models;
+
+namespace Tic_Tac_Toe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Models.TicTacToe ticTacToe;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            NewGame();
+        }
+
+        // Creating a new game by clearing the board, starting with player 'X' turn
+        public void NewGame()
+        {
+            ticTacToe = new TicTacToe();
+
+            uxGrid.IsEnabled = true;
+            uxTurn.Text = string.Format("{0} Turn", ticTacToe.Turn);
+
+            foreach (Button button in uxGrid.Children)
+            {
+                button.IsEnabled = true;
+                button.Content = string.Empty;
+            }
+        }
+
+        // Creating new game, 
+        private void uxNewGame_Click(object sender, RoutedEventArgs e)
+        {
+            NewGame();
+        }
+
+        // Closing the game
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        // When game is played
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Marks the current button and makes it disable
+            Button currentButton = (Button)sender;
+            currentButton.Style = ticTacToe.PlayerStyle;
+            currentButton.Content = ticTacToe.Turn;
+            currentButton.IsEnabled = false;
+
+            // Logs the position of the played button ex: 00, 01, 11, 21
+            var currentMove = currentButton.Tag.ToString().Remove(1, 1);
+            ticTacToe.PlayedMoves = currentMove;
+
+            // Checks for a winner
+            if (ticTacToe.GameOver() == true)
+            {
+                uxTurn.Text = string.Format("{0} is a winer", ticTacToe.Turn);
+                uxGrid.IsEnabled = false;
+            }
+            else
+            {
+                // Updates the player turn
+                ticTacToe.SwitchTurn();
+                uxTurn.Text = string.Format("{0} Turn", ticTacToe.Turn);
+            }
+
+        }
+    }
+}
---
+++ homework5/Tic-Tac-Toe/Models/TicTacToe.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace Tic_Tac_Toe.Models
+{
+    class TicTacToe
+    {
+        private char turn;
+        private List<string> xMoves;
+        private List<string> oMoves;
+        private Style xStyle = Application.Current.FindResource("XPlayerStyle") as Style;
+        private Style oStyle = Application.Current.FindResource("OPlayerStyle") as Style;
+
+
+        // Constructor starting with player X
+        public TicTacToe()
+        {
+            Turn = 'X';
+            xMoves = new List<string>();
+            oMoves = new List<string>();
+        }
+
+        // Property for turn assignment
+        public char Turn
+        {
+            get
+            {
+                return turn;
+            }
+
+            set
+            {
+                turn = value;
+            }
+        }
+
+
+        // Property to log the played moves
+        public string PlayedMoves
+        {
+
+            set
+            {
+                if (turn == 'X')
+                {
+                    xMoves.Add(value);
+                }
+                else
+                {
+                    oMoves.Add(value);
+                }
+            }
+        }
+
+        // Property to assign player's style
+        public Style PlayerStyle
+        {
+            get
+            {
+                if (turn == 'X')
+                {
+                    return xStyle;
+                }
+                else
+                {
+                    return oStyle;
+                }
+
+            }
+        }
+
+        // Switch player Turn
+        public void SwitchTurn()
+        {
+            if (Turn == 'X')
+            {
+                Turn = 'O';
+            }
+            else
+            {
+                Turn = 'X';
+            }
+        }
+
+        // Check to see game is over for current player (aka there is a winner)
+        public bool GameOver()
+        {
+            if (turn == 'X')
+            {
+                return WinnerCheck(xMoves);
+            }
+            else if (turn == 'O')
+            {
+                return WinnerCheck(oMoves);
+            }
+            return false;
+        }
+
+        // Algorithm to look for a winner, looking for posibilities of each tile
+        public bool WinnerCheck(List<string> playedMoves)
+        {
+            switch (playedMoves.Last())
+            {
+                case "00":
+
+                    if ((playedMoves.Contains("01") && playedMoves.Contains("02")) ||
+                        (playedMoves.Contains("10") && playedMoves.Contains("20")) ||
+                        (playedMoves.Contains("11") && playedMoves.Contains("22")))
+                        return true;
+                    break;
+
+                case "01":
+
+                    if ((playedMoves.Contains("00") && playedMoves.Contains("02")) ||
+                        (playedMoves.Contains("11")) && (playedMoves.Contains("21")))
+                        return true;
+                    break;
+
+
+                case "02":
+
+                    if ((playedMoves.Contains("00") && playedMoves.Contains("01")) ||
+                        (playedMoves.Contains("12") && playedMoves.Contains("22")) ||
+                        (playedMoves.Contains("11") && playedMoves.Contains("20")))
+                        return true;
+                    break;
+
+                case "10":
+
+                    if ((playedMoves.Contains("00") && playedMoves.Contains("20")) ||
+                    (playedMoves.Contains("11") && playedMoves.Contains("12")))
+                        return true;
+                    break;
+
+                case "11":
+
+                    if ((playedMoves.Contains("00") && playedMoves.Contains("22")) ||
+                        (playedMoves.Contains("01") && playedMoves.Contains("21")) ||
+                        (playedMoves.Contains("02") && playedMoves.Contains("20")) ||
+                        (playedMoves.Contains("10") && playedMoves.Contains("12")))
+                        return true;
+                    break;
+
+                case "12":
+
+                    if ((playedMoves.Contains("02") && playedMoves.Contains("22")) ||
+                        (playedMoves.Contains("10") && playedMoves.Contains("11")))
+                        return true;
+                    break;
+
+                case "20":
+
+                    if ((playedMoves.Contains("00") && playedMoves.Contains("10")) ||
+                        (playedMoves.Contains("21") && playedMoves.Contains("22")) ||
+                        (playedMoves.Contains("11") && playedMoves.Contains("02")))
+                        return true;
+                    break;
+
+                case "21":
+
+                    if ((playedMoves.Contains("20") && playedMoves.Contains("22")) ||
+                        (playedMoves.Contains("11") && playedMoves.Contains("01")))
+                        return true;
+                    break;
+
+                case "22":
+
+                    if ((playedMoves.Contains("12") && playedMoves.Contains("02")) ||
+                        (playedMoves.Contains("20") && playedMoves.Contains("21")) ||
+                        (playedMoves.Contains("11") && playedMoves.Contains("00")))
+                        return true;
+                    break;
+
+                default:
+                    break;
+            }
+
+            return false;
+        }
+
+    }
+
+
+}
